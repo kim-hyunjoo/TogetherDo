9499600e135ae7b93fcc6ed18d7c4965dfda5e1f
박재윤 <parkjaeyoon@bagjaeyun-ui-MacBookPro.local>
2022-11-14 00:27:35 +0900

    메인피드 구현

--- src/pages/main.js
+++ src/pages/main.js
@@ -1,10 +1,11 @@
 import React from "react";
-import Calendar from "../components/Calendar";
+import "../styles/Layout.css";
+import FeedScreen from "../components/FeedScreen";
 
 const Main = () => (
-    <div>
-        <div className="calendar">
-            <Calendar />
+    <div className="mainscreen">
+        <div className= "left">
+            <FeedScreen />
         </div>
     </div>
 );
